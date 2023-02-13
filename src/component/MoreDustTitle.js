9841460025f07446cc7ec063e33e9e170549073b
Kim-hyosun <jirehmuze@gmail.com>
2023-02-13 11:46:12 +0900

    v4_2

--- src/component/MoreDustTitle.js
+++ src/component/MoreDustTitle.js
@@ -2,7 +2,7 @@
 function MoreDustTitle() {
 	return (
 		<div className="maptitle">
-			<span className="title">모레의 미세먼지예보</span>
+			<span className="title">모레의 미세먼지</span>
 		{/* 	<div className="titleDate">
 				<span className="date">날짜</span>
 				<span className="time">기준시간</span>
